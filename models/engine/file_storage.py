2fe5ec52e81b9c8307629d22b8c3997caf09598a
njeveh <njeveh@gmail.com>
2024-07-20 17:32:16 +0300

    add updates

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,14 +15,12 @@ class FileStorage:
         Otherwise, returns the __objects dictionary.
         """
         if cls is not None:
-            if type(cls) == str:
-                cls = eval(cls)
-            cls_dict = {}
-            for k, v in self.__objects.items():
-                if type(v) == cls:
-                    cls_dict[k] = v
-            return cls_dict
-        return FileStorage.__objects
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    new_dict[key] = value
+            return new_dict
+        return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
